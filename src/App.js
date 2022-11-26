d4d3b562df2e45d16816339b291de45461da2179
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2022-11-26 13:36:24 -0600

    react-router routes and route s added

--- src/App.js
+++ src/App.js
@@ -1,52 +1,62 @@
-import React, {useState} from "react"
-import { v4 as uuidv4 } from 'uuid'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Header from "./components/Header";
-import FeedbackList from "./components/FeedbackList";
-import FeedbackData from "./data/FeedbackData";
-import FeedbackStats from "./components/FeedbackStats";
-import FeedbackForm from "./components/FeedbackForm";
-import About from "./pages/About";
-
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import About from './pages/About';
 
 // go back to https://www.udemy.com/course/react-front-to-back-2022/learn/lecture/29767836#content section 4.24 to integrate a cool animation
 
-function App(){
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
 
-    const [feedback, setFeedback] = useState(FeedbackData)
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
 
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
+    // the state is immutable, so we HAVE to make a copy of it in order to truly change it
+    // using the spread operator, we bring in all existing items in the list
+    // here we're setting feedback to be all existing items plus the newFeedback we just added
+    setFeedback([newFeedback, ...feedback]);
+  };
 
-        // the state is immutable, so we HAVE to make a copy of it in order to truly change it
-        // using the spread operator, we bring in all existing items in the list
-        // here we're setting feedback to be all existing items plus the newFeedback we just added
-        setFeedback([newFeedback, ...feedback])
+  const deleteFeedback = (id) => {
+    if (window.confirm('are you sure you want to delete?')) {
+      // filters out the item we're deleting if it's a match
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
+  };
 
-    const deleteFeedback = (id) => {
-        if(window.confirm('are you sure you want to delete?')){
-            // filters out the item we're deleting if it's a match
-            setFeedback(feedback.filter((item)=> item.id !== id))
-        }
-    }
+  return (
+    <Router>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+                // parent element still required
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                {/* feedback={feedback} -> 1st feedback == the name of the prop, 2nd feedback == what we named our state  */}
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
 
-    return (
-        <Router>
-        <Header />
-        <div className="container">
-            <Route exact path='/'>
-            <FeedbackForm handleAdd={addFeedback}/>
-            <FeedbackStats feedback={feedback} />
-            {/* feedback={feedback} -> 1st feedback == the name of the prop, 2nd feedback == what we named our state  */}
-            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-            </Route>
-
-            <Route path='/about' component={About}/>
-        </div>
-        </Router>
-    )
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Card from '../components/shared/Card'
 
 function About() {
-  return (
-    <div>About</div>
-  )
+  return <Card>
+    <div className='about'>
+      <h1>About This Project</h1>
+      <p>This is a feedback app, from a udemy course by Brad Traversy on creating a React app</p>
+      <p>Version: 1.0.0</p>
+
+      <p>
+        <a href="/">back to home</a>
+      </p>
+
+    </div>
+  </Card>
 }
 
 export default About
